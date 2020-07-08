1cf7da08724099e23f5dc78e53406c88f284a25b
Goran Gogsi <dragutinovic.g@gmail.com>
2020-07-08 11:07:37 -0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+//const myHeading = document.querySelector('h1');
+//myHeading.textContent = 'My coffee needs more creamer!';
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/the-sentinel.jpg') {
+      myImage.setAttribute ('src','images/chinatown.jpg');
+    } else {
+      myImage.setAttribute ('src','images/the-sentinel.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h2');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'I will get right on it it, ' + myName;
+    }
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'I will get right on it it, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
